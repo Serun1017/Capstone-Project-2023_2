212baf691ecbde737aeb7bb6af01feda15165016
ToroidalFox <93528864+ToroidalFox@users.noreply.github.com>
2023-11-16 18:24:09 +0900

    example

--- undis/__main__.py
+++ undis/__main__.py
@@ -1,8 +1,31 @@
 import customtkinter
 
+
 customtkinter.set_appearance_mode("system")
 
-app = customtkinter.CTk()
+
+def test_callback():
+    print("what")
+
+
+class ResultFrame(customtkinter.CTkScrollableFrame):
+    def __init__(self, master, **kwargs):
+        super().__init__(master=master, **kwargs)
+
+        self.button = customtkinter.CTkButton(self, text="test", command=test_callback)
+        self.button.pack()
+
+
+class App(customtkinter.CTk):
+    def __init__(self):
+        super().__init__()
+        self.title("undis")
+
+        self.frame = ResultFrame(self)
+        self.frame.pack()
+
+
+app = App()
 app.title("undis")
 app.geometry("400x300")
 
