d4d022ede5df5d1c153bf46bdf789c1834666f4c
Hotties5060 <changmin2580@gmail.com>
2023-11-17 15:22:00 +0900

    버튼 구상만 함

--- undis/__main__.py
+++ undis/__main__.py
@@ -1,32 +1,55 @@
+from tkinter import Canvas
 import customtkinter
 
 
 customtkinter.set_appearance_mode("system")
+
+Width = 1024
+Height = 512
 
 
 def test_callback():
     print("what")
 
 
+def get_x_and_y(event):
+    global lasx, lasy
+    lasx, lasy = event.x, event.y
+    pass
+
+
+def make_button(self):
+    self.playbutton = customtkinter.CTkButton(self, text="play", command=test_callback)
+    self.playbutton.place(x=50)
+    self.playbutton.pack(side="left", anchor="nw")
+
+    pass
+
+
 class ResultFrame(customtkinter.CTkScrollableFrame):
     def __init__(self, master, **kwargs):
-        super().__init__(master=master, **kwargs)
-
-        self.button = customtkinter.CTkButton(self, text="test", command=test_callback)
-        self.button.pack()
+        super().__init__(master=master, width=Width / 2, height=Height, **kwargs)  # type: ignore
 
 
 class App(customtkinter.CTk):
     def __init__(self):
         super().__init__()
         self.title("undis")
+        self.resizable(True, True)
+        make_button(self)
 
         self.frame = ResultFrame(self)
+        self.make_canvas()
         self.frame.pack()
+
+    def make_canvas(self):
+        self.canvas = Canvas(self, width=Width / 2, height=Height + 100, bg="black")
+        self.canvas.pack(
+            side="left",
+            fill="none",
+        )
 
 
 app = App()
-app.title("undis")
-app.geometry("400x300")
-
+app.geometry("1024x512")
 app.mainloop()
